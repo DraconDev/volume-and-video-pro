6b4887f316d1b38b23aaa9937d325805eaa9cd96
DraconDev <dracsharp@gmail.com>
2025-02-07 19:25:35 +0000

    fix(styles): Add vertical spacing to range input Adds `my-3` to range input for spacing and fixes duplicate webkit-slider-thumb.

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
         permissions: ["storage", "tabs"],
         host_permissions: ["<all_urls>"],
         name: "Volume & Video Master 1000%",
-        version: "1.1.32",
+        version: "1.1.33",
         description: "Volume & Video Master",
         background: {
             service_worker: "entrypoints/background.ts",
