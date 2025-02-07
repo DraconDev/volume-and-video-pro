fecabf02ac3256be86f3d8db1d2a0b07c46605ec
DraconDev <dracsharp@gmail.com>
2025-02-07 22:48:34 +0000

    feat(media-manager): Add AudioSettings import

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
         permissions: ["storage", "tabs"],
         host_permissions: ["<all_urls>"],
         name: "Volume & Video Master 1000%",
-        version: "1.4.30",
+        version: "1.4.31",
         description: "Volume & Video Master",
         background: {
             service_worker: "entrypoints/background.ts",
