6be47d1edffa1010a7959ad7d2f3d1eea707775a
DraconDev <dracsharp@gmail.com>
2025-02-07 20:52:23 +0000

    refactor: Rename  default  to  disabled  for clarity

--- src/components/SettingsToggle.tsx
+++
@@ -1,8 +0,0 @@
-// ...existing code...
-<Toggle
-  checked={!isDisabled} // Updated from !isDefault
-  onChange={handleToggle}
-  aria-label={isDisabled ? 'Enable extension for this site' : 'Disable extension for this site'} // Updated text
-/>
-<span>{isDisabled ? 'Disabled' : 'Enabled'}</span> // Updated text
-// ...existing code...
