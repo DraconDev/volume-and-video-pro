467cebadcc32629295dc29a0681d111c3f65cba3
DraconDev <dracsharp@gmail.com>
2025-04-07 00:59:01 +0100

    feat(manifest): add content script and decrement version The changes include adding a content script to the manifest and decrementing the version number from 1.4.51 to 1.4.50.

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,32 +1,40 @@
 import { defineConfig } from "wxt";
 
 export default defineConfig({
-    manifest: {
-        permissions: ["storage", "tabs"],
-        host_permissions: ["<all_urls>"],
-        name: "Volume & Video Master 1000%",
-        version: "1.4.51",
-        description: "Volume & Video Master",
-        background: {
-            service_worker: "entrypoints/background.ts",
-            type: "module",
-        },
-
-        action: {
-            default_icon: {
-                16: "icon/16.png",
-                32: "icon/32.png",
-                48: "icon/48.png",
-                128: "icon/128.png",
-            },
-        },
-
-        web_accessible_resources: [
-            {
-                resources: ["assets/*"],
-                matches: ["<all_urls>"],
-            },
-        ],
+  manifest: {
+    permissions: ["storage", "tabs"],
+    host_permissions: ["<all_urls>"],
+    name: "Volume & Video Master 1000%",
+    version: "1.4.52",
+    description: "Volume & Video Master",
+    background: {
+      service_worker: "entrypoints/background.ts",
+      type: "module",
     },
-    modules: ["@wxt-dev/module-react"],
+
+    action: {
+      default_icon: {
+        16: "icon/16.png",
+        32: "icon/32.png",
+        48: "icon/48.png",
+        128: "icon/128.png",
+      },
+    },
+
+    web_accessible_resources: [
+      {
+        resources: ["assets/*"],
+        matches: ["<all_urls>"],
+      },
+    ],
+  },
+  modules: ["@wxt-dev/module-react"],
+  contentScripts: [
+    {
+      entrypoint: 'entrypoints/content.ts', // Reference the entrypoint file
+      matches: ["<all_urls>"],
+      allFrames: true, // Use camelCase for WXT options
+      runAt: "document_start", // Use camelCase for WXT options
+    }
+  ],
 });
