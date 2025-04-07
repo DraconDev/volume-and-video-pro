82f7d49faeb0b8456089ce96b6e77ba5c785c399
DraconDev <dracsharp@gmail.com>
2025-04-07 01:00:13 +0100

    fix(content-script): use .ts extension for content script entrypoint

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     permissions: ["storage", "tabs"],
     host_permissions: ["<all_urls>"],
     name: "Volume & Video Master 1000%",
-    version: "1.4.56",
+    version: "1.4.57",
     description: "Volume & Video Master",
     background: {
       service_worker: "entrypoints/background.ts",
@@ -30,7 +30,7 @@ export default defineConfig({
     content_scripts: [
       {
         matches: ["<all_urls>"],
-        js: ["entrypoints/content.js"],
+        js: ["entrypoints/content.ts"],
         all_frames: true, // Inject into all frames (including iframes)
         run_at: "document_start", // Inject as early as possible
       },
