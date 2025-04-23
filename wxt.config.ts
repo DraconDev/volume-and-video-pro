e3312a69e02fb5db1b48b9f5a1fd7803667bb916
DraconDev <dracsharp@gmail.com>
2025-04-23 12:55:35 +0100

    feat: bump version to 2.0.0

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     permissions: ["storage", "tabs"],
     host_permissions: ["<all_urls>"],
     name: "Volume & Video Master 1000%",
-    version: "1.6.2",
+    version: "2.0.0",
     description: "Volume & Video Master",
     background: {
       service_worker: "entrypoints/background.ts",
